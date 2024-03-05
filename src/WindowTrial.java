a9f20789a3ea142ee71d3633919c0f51231c3a48
VADUMr <115543904+VADUMr@users.noreply.github.com>
2024-03-05 18:22:34 +0200

    v1.4

--- src/WindowTrial.java
+++ src/WindowTrial.java
@@ -3,6 +3,9 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
 import java.awt.*;
 
 public class WindowTrial extends JFrame {
@@ -14,6 +17,22 @@ public class WindowTrial extends JFrame {
         ImageIcon icon = new ImageIcon("src/icon.png");
         panel.add(new JLabel(icon));
         panel.add(new JButton("Ще кнопка", new ImageIcon("src/icon.png")));
+
+        JTextField textField = new JTextField("Текстове поле", 20);
+        textField.setCaretColor(Color.RED);
+        textField.setBackground(Color.yellow);
+        textField.setHorizontalAlignment(JTextField.CENTER);
+        JPasswordField passwordField = new JPasswordField(20);
+        passwordField.setEchoChar('?');
+        passwordField.setText("пароль");
+        JTextArea textArea = new JTextArea(3, 20);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        for (int i = 0; i <= 3; i++)
+            textArea.append("Область для вводу тексту");
+        panel.add(textField, BorderLayout.NORTH);
+        panel.add(textArea);
+        panel.add(passwordField, BorderLayout.SOUTH);
         setContentPane(panel);
         setSize(800, 800);
     }
