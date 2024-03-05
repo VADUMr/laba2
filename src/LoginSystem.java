b8e65baff9e6eb881c8e83c08304ea5225acce47
VADUMr <115543904+VADUMr@users.noreply.github.com>
2024-03-05 19:25:12 +0200

    v1.4.2

---
+++ src/LoginSystem.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class LoginSystem extends JFrame {
+    LoginSystem(){
+        super("Вікно входу");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+    }
+}
