51847b1c4670d999e10633524e62451136475d5f
VADUMr <115543904+VADUMr@users.noreply.github.com>
2024-03-05 17:56:55 +0200

    v1.2.2

--- src/WindowTrial.java
+++ src/WindowTrial.java
@@ -3,7 +3,6 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-
 import java.awt.*;
 
 public class WindowTrial extends JFrame {
@@ -11,14 +10,11 @@ public class WindowTrial extends JFrame {
         super("Пробне вікно");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());//GridLayout(2,3,5,10));
-        panel.add(new JButton("Кнопка"));
-        panel.add(new JButton("+"));
-        panel.add(new JButton("-"));
-        panel.add(new JButton("Кнопка з довгим написом"));
-        panel.add(new JButton("Ще кнопка"));
+        panel.setLayout(new FlowLayout());
         ImageIcon icon = new ImageIcon("src/icon.png");
         panel.add(new JLabel(icon));
-        setContentPane(panel);setSize(400, 600);
+        panel.add(new JButton("Ще кнопка", new ImageIcon("src/icon.png")));
+        setContentPane(panel);
+        setSize(800, 800);
     }
 }
