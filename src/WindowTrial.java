8a06b36be61a753e217c7680f6b34cb92129731b
VADUMr <115543904+VADUMr@users.noreply.github.com>
2024-03-05 17:38:58 +0200

    v1.2.2

--- src/WindowTrial.java
+++ src/WindowTrial.java
@@ -1,6 +1,9 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 import java.awt.*;
 
 public class WindowTrial extends JFrame {
@@ -14,6 +17,8 @@ public class WindowTrial extends JFrame {
         panel.add(new JButton("-"));
         panel.add(new JButton("Кнопка з довгим написом"));
         panel.add(new JButton("Ще кнопка"));
+        ImageIcon icon = new ImageIcon("src/icon.png");
+        panel.add(new JLabel(icon));
         setContentPane(panel);setSize(400, 600);
     }
 }
