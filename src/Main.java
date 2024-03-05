a4cc8bf9f4bec1cb4803d7ec02c4a1922ed1dfd2
VADUMr <115543904+VADUMr@users.noreply.github.com>
2024-03-05 17:21:06 +0200

    v1.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        JFrame myWindow = new WindowTrial ();
+        myWindow.setVisible (true);
         }
-    }
 }
