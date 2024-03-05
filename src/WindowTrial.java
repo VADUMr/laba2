415ffb5ab469f18f8de87979d959288e6991cf14
VADUMr <115543904+VADUMr@users.noreply.github.com>
2024-03-05 17:26:10 +0200

    v1.2

--- src/WindowTrial.java
+++ src/WindowTrial.java
@@ -1,9 +1,19 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.*;
 
 public class WindowTrial extends JFrame {
     WindowTrial() {
         super("Пробне вікно");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(250, 100);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,3,5,10));
+        panel.add(new JButton("Кнопка"));
+        panel.add(new JButton("+"));
+        panel.add(new JButton("-"));
+        panel.add(new JButton("Кнопка з довгим написом"));
+        panel.add(new JButton("Ще кнопка"));
+        setContentPane(panel);setSize(400, 600);
     }
 }
